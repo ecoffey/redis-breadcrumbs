65b57679ea703cca36d71508f7b6c02ba34f5113
Eoin Coffey <ecoffey@gnipcentral.com>
2012-12-06 18:54:40 -0700

    Make 'can own a key' pass

--- lib/redis-breadcrumbs/breadcrumb.rb
+++ lib/redis-breadcrumbs/breadcrumb.rb
@@ -1,8 +1,14 @@
 module Redis
   class Breadcrumb
     class << self
+      attr_accessor :owned_keys
+
       def tracked_in *args
         args.length > 0 ? @tracked_in = args[0] : @tracked_in
+      end
+
+      def owns key
+        (@owned_keys ||= []) << key
       end
     end
   end
--- test/breadcrumb_test.rb
+++ test/breadcrumb_test.rb
@@ -1,11 +1,17 @@
 require 'test_helper'
 
 describe 'Redis::Breadcrumb' do
+  class TestBreadcrumb < Redis::Breadcrumb
+    tracked_in 'tracking_key'
+
+    owns :a_owned_key
+  end
+
   it 'can record a key to track in' do
-    class TestBreadcrumb < Redis::Breadcrumb
-      tracked_in 'tracking_key'
-    end
     assert_equal 'tracking_key', TestBreadcrumb.tracked_in
   end
-end
 
+  it 'can own a key' do
+    assert_equal [:a_owned_key], TestBreadcrumb.owned_keys
+  end
+end
