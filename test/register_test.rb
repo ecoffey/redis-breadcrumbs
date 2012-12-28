a71d07e6f6d92fa519423622d5bf0542e22308b9
Eoin Coffey <ecoffey@gmail.com>
2012-12-27 21:20:50 -0700

    Add test 'can register owned keys for a specific object'

---
+++ test/register_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+describe 'Redis::Breadcrumb' do
+  before do
+    Redis::Breadcrumb.redis = MockRedis.new
+  end
+
+  it 'can register owned keys for a specific object' do
+    class OwnedBreadcrumb < Redis::Breadcrumb
+      owns 'widget:<id>'    
+    end
+
+    obj = Object.new
+    class << obj
+      def id; "foo"; end
+    end
+
+    breadcrumb = OwnedBreadcrumb.register(obj)
+
+    assert_equal [["del", "widget:foo"]], breadcrumb.tracked_keys
+  end
+end
