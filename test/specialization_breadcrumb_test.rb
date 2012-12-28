f896e70d8eec81a8f553fccf70b03e203f3c43ec
Eoin Coffey <ecoffey@gmail.com>
2012-12-27 20:30:14 -0700

    Add test 'can specialize owned key names'

--- test/specialization_breadcrumb_test.rb
+++ test/specialization_breadcrumb_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 describe 'Redis::Breadcrumb' do
+  class SpecializedBreadcrumb < Redis::Breadcrumb
+    tracked_in 'widget:<id>:tracking'
+
+    owns 'widget:<id>'
+  end
+
   before do
     redis = MockRedis.new
 
@@ -8,16 +14,23 @@ describe 'Redis::Breadcrumb' do
   end
 
   it 'can specialize tracked_in key name' do
-    class SpecializedBreadcrumb < Redis::Breadcrumb
-      tracked_in 'widget:<id>'
-    end
-
     obj = Object.new
     class << obj
       def id; "foo"; end
     end
 
-    assert_equal 'widget:foo', SpecializedBreadcrumb.new(obj).tracked_in
+    assert_equal 'widget:foo:tracking', SpecializedBreadcrumb.new(obj).tracked_in
+  end
+
+  it 'can specialize owned key names' do
+    obj = Object.new
+    class << obj
+      def id; "foo"; end
+    end
+
+    breadcrumb = SpecializedBreadcrumb.new obj
+
+    assert_equal ['widget:foo'], breadcrumb.owned_keys
   end
 
 end
