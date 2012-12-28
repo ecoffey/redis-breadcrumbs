601faa93130f5ad7e8c12de589d708bf19e7ece3
Eoin Coffey <ecoffey@gmail.com>
2012-12-28 09:33:48 -0700

    Add test 'can register member of set keys for a specific object'

--- test/register_test.rb
+++ test/register_test.rb
@@ -21,4 +21,21 @@ describe 'Redis::Breadcrumb' do
 
     assert_equal [["del", "widget:foo"]], breadcrumb.tracked_keys
   end
+
+  it 'can register member of set keys for a specific object' do
+    class MemberOfSetBreadcrumb < Redis::Breadcrumb
+      tracked_in 'widget:<id>:tracking'
+
+      member_of_set "<id>" => :a_set_of_things
+    end
+
+    obj = Object.new
+    class << obj
+      def id; "foo"; end
+    end
+
+    breadcrumb = MemberOfSetBreadcrumb.register(obj)
+
+    assert_equal [["srem", "a_set_of_things", "foo"]], breadcrumb.tracked_keys
+  end
 end
