83979992779d73e46ba0a2e22e2dc5b2890f8248
Eoin Coffey <ecoffey@gmail.com>
2013-01-07 19:51:08 -0700

    Dump mock_redis version for travis CI debugging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'mock_redis'
+require 'mock_redis/version'
 require 'minitest/autorun'
 
 require 'redis-breadcrumbs'
+
+puts "mock_redis: #{MockRedis::VERSION}"
