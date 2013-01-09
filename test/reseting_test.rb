4814f5f540180c1bc832bc6c10dd7a389d37cd5a
Eoin Coffey <ecoffey@gmail.com>
2013-01-08 20:38:06 -0700

    Add 'by default resets member of sorted set keys with clean command' test

--- test/reseting_test.rb
+++ test/reseting_test.rb
@@ -32,4 +32,21 @@ describe 'Redis::Breadcrumb' do
     assert_equal 0, MemberOfSetResetWithClean.redis.scard('a_set')
     refute MemberOfSetResetWithClean.redis.sismember 'a_set', 'blah'
   end
+
+  it 'by default resets member of sorted set keys with clean command' do
+    class MemberOfZsetResetWithClean < Redis::Breadcrumb
+      member_of_zset :blah => :a_set, :reset => true
+    end
+
+    MemberOfZsetResetWithClean.redis.zadd 'a_set', 1, 'blah'
+
+    assert_equal 1, MemberOfZsetResetWithClean.redis.zcard('a_set')
+    assert MemberOfZsetResetWithClean.redis.zrank 'a_set', 'blah'
+
+    MemberOfZsetResetWithClean.reset!
+
+    assert_equal 0, MemberOfZsetResetWithClean.redis.zcard('a_set')
+    refute MemberOfZsetResetWithClean.redis.zrank 'a_set', 'blah'
+  end
+
 end
